bdea7aef877c7014c2b18c963ed33f004df46362
Soorriamoorthy Saruva <soorria.ss@gmail.com>
2019-08-29 09:32:33 +1000

    Set server to serve static files when in production

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const path = require('path');
 
-require('dotenv').config(); // Get environment variables
-// Initialise app
+// Get environment variables
+require('dotenv').config();
+
+// Initialise express app
 const app = express();
 
 // Add JSON body parser
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+} else {
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+}
 
 app.use('/api/todos', require('./routes/api/todos'));
 
